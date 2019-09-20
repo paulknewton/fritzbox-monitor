9f0a55364ec1bca315263dd949c2687079464caa
Paul Newton <paul@topolino.org>
2019-09-21 00:50:52 +0200

    Support python 2

---
+++ tests/convert_pickle.py
@@ -0,0 +1,14 @@
+import pickle
+
+def change_pickle_protocol(filepath,protocol=2):
+    with open(filepath,'rb') as f:
+        obj = pickle.load(f)
+    with open(filepath,'wb') as f:
+        pickle.dump(obj,f,protocol=protocol)
+
+
+if __name__ == '__main__':
+    pkl_filenmae = "expected_df.pkl"
+
+    print("Converting %s from new pickle format to old pickle format")
+    change_pickle_protocol("expected_df.pkl")
